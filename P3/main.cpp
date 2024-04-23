75b603a833e33165b217189e3a4895dd014728c7
FIRST_NAME LAST_NAME <juamon05@ucm.es>
2024-04-23 17:53:15 +0200

    Bayes 100% terminado

--- P3/main.cpp
+++ P3/main.cpp
@@ -14,6 +14,15 @@ using Eigen::Vector4d;
 using namespace std;
 
 
+struct m_y_c
+{
+    Vector4d m1;
+    Vector4d m2;
+    Matrix4d c1;
+    Matrix4d c2;
+};
+
+
 vector<Vector4d> readIrisData(const string& filename) {
     vector<Vector4d> data;
     ifstream file(filename);
@@ -78,9 +87,21 @@ Matrix4d calcular_c(vector<Vector4d> datosClase, Vector4d m){
     return covarianza;
 }
 
+double calcular_distancia(Vector4d x, Vector4d m, Matrix4d c){
+    double distancia; 
+
+    Vector4d dif;
+    dif = x - m;
+
+    distancia = dif.transpose() * c.completeOrthogonalDecomposition().pseudoInverse() * dif; 
+
+    return distancia;
+
+}
 
 
-void bayes(vector<Vector4d> irisData) {
+
+m_y_c bayes_hacer_clasificacion(vector<Vector4d> irisData) {
     vector<Vector4d> clase1;
     vector<Vector4d> clase2;
 
@@ -106,6 +127,16 @@ void bayes(vector<Vector4d> irisData) {
     cout << "c1: " << endl << c1 << endl;
     cout << "c2: " << endl << c2 << endl;
 
+    return {m1, m2, c1, c2};
+
+}
+
+int bayes_clasificar(m_y_c myc, Vector4d ejemplo){
+
+    if (calcular_distancia(ejemplo, myc.m1, myc.c1) > calcular_distancia(ejemplo, myc.m2, myc.c2))
+        return 2;
+    else 
+        return 1;
 }
 
 int main() {
@@ -115,11 +146,25 @@ int main() {
     cout << "Datos Leidos:" << endl << "_____________" << endl;
     for (int i = 0; i < irisData.size(); i++) {
         cout << i << ": " << irisData[i].transpose() << endl;
-        if (i == 50) {cout << endl;}
+        if (i == 49) {cout << endl;}
     }
     cout << "____________________" << endl;
 
-    bayes(irisData);
+    vector<Vector4d> ejemplo1 = readIrisData("TestIris01.txt");
+    vector<Vector4d> ejemplo2 = readIrisData("TestIris02.txt");
+    vector<Vector4d> ejemplo3 = readIrisData("TestIris03.txt");
+
+
+    m_y_c myc = bayes_hacer_clasificacion(irisData);
+    int ej1 = bayes_clasificar(myc, ejemplo1[0]);
+    int ej2 = bayes_clasificar(myc, ejemplo2[0]);
+    int ej3 = bayes_clasificar(myc, ejemplo3[0]);
+    
+    cout << endl;
+    cout << "Ej1 es de la clase: " << ej1 << endl;
+    cout << "Ej2 es de la clase: " << ej2 << endl;
+    cout << "Ej3 es de la clase: " << ej3 << endl;
+
 
 
 
