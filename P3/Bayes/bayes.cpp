bf8421430be73862c78a7ccefa9477899e31f5a9
AlvaroDelCampoUCM <alcamp03@ucm.es>
2024-05-02 11:02:46 +0200

    Lloyd terminado y recolocacion de carpetas y archivos

---
+++ P3/Bayes/bayes.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+//Librería de matrices
+//!Para compilar hay que usar g++ -I ../eigen-3.4.0/ bayes.cpp -o bayes.exe
+#include <Eigen/Dense>
+ 
+using Eigen::Matrix4d;
+using Eigen::Vector4d;
+
+using namespace std;
+
+
+struct m_y_c {
+    Vector4d m1;
+    Vector4d m2;
+    Matrix4d c1;
+    Matrix4d c2;
+};
+
+
+vector<Vector4d> readIrisData(const string& filename) {
+    vector<Vector4d> data;
+    ifstream file(filename);
+
+    if (!file.is_open()) {
+        cerr << "Error al abrir el archivo " << filename << endl;
+        return data;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        Vector4d entry;
+        char comma;
+
+        ss >> entry(0) >> comma
+            >> entry(1) >> comma
+            >> entry(2) >> comma
+            >> entry(3) >> comma;
+
+        data.push_back(entry);
+    }
+
+    file.close();
+    return data;
+}
+
+
+
+Vector4d calcular_m(vector<Vector4d> datosClase){
+    Vector4d m = { 0, 0, 0, 0 };
+    for (auto dato : datosClase) {
+        m += dato;
+    }
+    m = m / 50;
+    
+    
+    return m;
+}
+
+Matrix4d calcular_c(vector<Vector4d> datosClase, Vector4d m){
+    
+    vector<Vector4d> diferencias;
+    for (auto x : datosClase){
+    Vector4d dif;
+        dif = x - m;
+        diferencias.push_back(dif);
+    }
+
+
+    Matrix4d covarianza {
+      {0, 0, 0, 0},
+      {0, 0, 0, 0},
+      {0, 0, 0, 0},
+      {0, 0, 0, 0},};
+    for(auto d : diferencias){
+        covarianza = covarianza + ( d * d.transpose());
+    }
+    covarianza = covarianza / 50;
+
+
+    return covarianza;
+}
+
+double calcular_distancia(Vector4d x, Vector4d m, Matrix4d c){
+    double distancia; 
+
+    Vector4d dif;
+    dif = x - m;
+
+    distancia = dif.transpose() * c.completeOrthogonalDecomposition().pseudoInverse() * dif; 
+
+    return distancia;
+
+}
+
+
+
+m_y_c entrenar_bayes(vector<Vector4d> irisData) {
+    vector<Vector4d> clase1;
+    vector<Vector4d> clase2;
+
+    for (int i = 0; i < 50; i++) {
+        clase1.push_back(irisData[i]);
+    }
+
+    for (int i = 50; i < 100; i++) {
+        clase2.push_back(irisData[i]);
+    }
+    
+    Vector4d m1 = calcular_m(clase1);
+    Vector4d m2 = calcular_m(clase2);
+
+
+    cout << endl << endl;
+    cout << "m1: " << endl << m1 << endl;
+    cout << "m2: " << endl << m2 << endl;
+    cout << endl;
+
+    Matrix4d c1 = calcular_c(clase1, m1);
+    Matrix4d c2 = calcular_c(clase2, m2);
+    cout << "c1: " << endl << c1 << endl;
+    cout << "c2: " << endl << c2 << endl;
+
+    return {m1, m2, c1, c2};
+
+}
+
+int bayes_clasificar(m_y_c myc, Vector4d ejemplo){
+
+    if (calcular_distancia(ejemplo, myc.m1, myc.c1) > calcular_distancia(ejemplo, myc.m2, myc.c2))
+        return 2;
+    else 
+        return 1;
+}
+
+int main() {
+    vector<Vector4d> irisData = readIrisData("../lectura_archivos/Iris2Clases.txt");
+
+    // Imprimir los datos leídos
+    cout << "Datos Leidos:" << endl << "_____________" << endl;
+    for (int i = 0; i < irisData.size(); i++) {
+        cout << i << ": " << irisData[i].transpose() << endl;
+        if (i == 49) {cout << endl;}
+    }
+    cout << "____________________" << endl;
+
+    vector<Vector4d> ejemplo1 = readIrisData("../lectura_archivos/TestIris01.txt");
+    vector<Vector4d> ejemplo2 = readIrisData("../lectura_archivos/TestIris02.txt");
+    vector<Vector4d> ejemplo3 = readIrisData("../lectura_archivos/TestIris03.txt");
+
+
+    m_y_c myc = entrenar_bayes(irisData);
+    int ej1 = bayes_clasificar(myc, ejemplo1[0]);
+    int ej2 = bayes_clasificar(myc, ejemplo2[0]);
+    int ej3 = bayes_clasificar(myc, ejemplo3[0]);
+    
+    cout << endl;
+    cout << "Ej1 es de la clase: " << ej1 << endl;
+    cout << "Ej2 es de la clase: " << ej2 << endl;
+    cout << "Ej3 es de la clase: " << ej3 << endl;
+
+
+    //!Para compilar hay que usar g++ -I ../eigen-3.4.0/ bayes.cpp -o bayes.exe
+    // Documentación: https://eigen.tuxfamily.org/dox/group__TutorialMatrixClass.html
+
+    return 0;
+}
---
+++ P3/Lloyd/lloyd.cpp
@@ -0,0 +1,167 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+//#include <cstdlib> //-> para rand()
+#include <ctime>
+
+//Librería de matrices
+//!Para compilar hay que usar g++ -I ../eigen-3.4.0/ lloyd.cpp -o lloyd.exe
+#include <Eigen/Dense>
+ 
+using Eigen::Matrix4d;
+using Eigen::Vector4d;
+
+using namespace std;
+
+double tolerancia = 0.0000000001;
+int maximasIteraciones = 10;
+double razonAprendizaje = 0.1; //(gamma)
+
+
+vector<Vector4d> readIrisData(const string& filename) {
+    vector<Vector4d> data;
+    ifstream file(filename);
+
+    if (!file.is_open()) {
+        cerr << "Error al abrir el archivo " << filename << endl;
+        return data;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        Vector4d entry;
+        char comma;
+
+        ss >> entry(0) >> comma
+            >> entry(1) >> comma
+            >> entry(2) >> comma
+            >> entry(3) >> comma;
+
+        data.push_back(entry);
+    }
+
+    file.close();
+    return data;
+}
+
+//inicialización aleatoria de los centros
+vector<Vector4d> ini_c_rand(int numCentros) {
+    vector<Vector4d> centros;
+    Vector4d centro;
+
+    srand(time(nullptr));
+    for (int i = 0; i < numCentros; i++) {
+        
+        centro(0) = double(rand() % 70) / 10;
+        centro(1) = double(rand() % 70) / 10;
+        centro(2) = double(rand() % 70) / 10;
+        centro(3) = double(rand() % 70) / 10;
+        centros.push_back(centro);
+    }
+    return centros;
+}
+
+//inicialización definida de los centros (según el apéndice del enunciado)
+vector<Vector4d> ini_c_forced() {
+    vector<Vector4d> centros;
+
+    Vector4d c1(4.6, 3.0, 4.0, 0.0);
+    Vector4d c2(6.8, 3.4, 4.6, 0.7);
+
+    centros.push_back(c1);
+    centros.push_back(c2);
+
+    return centros;
+}
+
+//Calcula la distancia euclidiana entre dos vectores
+double calcularD(Vector4d x, Vector4d c) {
+    return sqrt( pow(x(0) - c(0), 2) 
+                + pow(x(1) - c(1), 2) 
+                + pow(x(2) - c(2), 2) 
+                + pow(x(3) - c(3), 2)
+                );
+}
+
+//Calcula el centro más cercano a un vector x 
+int calcularC_mas_proximo(Vector4d x, vector<Vector4d> centros) {
+    int i_cMasProximo = 0;
+    double distancia_cMasProximo = 10000;
+    for(int i = 0; i < centros.size(); i++) {
+        double distancia = calcularD(x, centros.at(i));
+        if (distancia < distancia_cMasProximo) {
+            i_cMasProximo = i;
+            distancia_cMasProximo = distancia;
+        }
+    }
+    
+    return i_cMasProximo;
+}
+
+//Actualiza el centro que se le pase para que se acerque al vector x
+void actualizarC(Vector4d& c, Vector4d x) {
+    c = c + (razonAprendizaje * (x-c));
+}
+
+
+vector<Vector4d> entrenar_lloyd(vector<Vector4d> irisData) {
+
+    vector<Vector4d> centros = ini_c_forced();
+
+    cout <<"c1= " << centros.at(0).transpose()  << " | c2= " << centros.at(1).transpose() << "\n\n"; 
+    int i_cMasProximo;
+    for (int j = 0; j < maximasIteraciones; j++) {
+        for (int i = 0; i < irisData.size(); i++ ){
+            i_cMasProximo = calcularC_mas_proximo(irisData.at(i), centros);
+            cout << "Mas proximo = " << i_cMasProximo+1 << " | ";
+            actualizarC(centros.at(i_cMasProximo), irisData.at(i));
+            cout << "new_c" << i_cMasProximo+1 <<"= " << centros.at(i_cMasProximo).transpose() << endl;
+        }
+    }
+    cout << "__________________________" << endl << endl;
+    cout << "Centros finales:" << endl;
+    for (int i = 0; i < centros.size(); i++) {
+        cout << "c" << i+1 << "= " << centros.at(i).transpose() << endl;
+    }
+    
+    return centros;
+}
+
+
+void clasificar_lloyd(vector<Vector4d> centros, Vector4d ejemplo1, Vector4d ejemplo2, Vector4d ejemplo3) {
+    int cMasProximoEj1 = calcularC_mas_proximo(ejemplo1, centros);
+    cout << "\nEl Ejemplo 1 pertenece a la clase " << cMasProximoEj1+1 << endl;
+    int cMasProximoEj2 = calcularC_mas_proximo(ejemplo2, centros);
+    cout << "El Ejemplo 2 pertenece a la clase " << cMasProximoEj2+1 << endl;
+    int cMasProximoEj3 = calcularC_mas_proximo(ejemplo3, centros);
+    cout << "El Ejemplo 3 pertenece a la clase " << cMasProximoEj3+1 << endl;
+}
+
+
+int main() {
+    vector<Vector4d> irisData = readIrisData("../lectura_archivos/Iris2Clases.txt");
+
+    // Imprimir los datos leídos
+    cout << "Datos Leidos:" << endl << "_____________" << endl;
+    for (int i = 0; i < irisData.size(); i++) {
+        cout << i << ": " << irisData[i].transpose() << endl;
+        if (i == 49) {cout << endl;}
+    }
+    cout << "____________________" << endl;
+
+    vector<Vector4d> ejemplo1 = readIrisData("../lectura_archivos/TestIris01.txt");
+    vector<Vector4d> ejemplo2 = readIrisData("../lectura_archivos/TestIris02.txt");
+    vector<Vector4d> ejemplo3 = readIrisData("../lectura_archivos/TestIris03.txt");
+
+    vector<Vector4d> centros = entrenar_lloyd(irisData);
+    clasificar_lloyd(centros, ejemplo1[0], ejemplo2[0], ejemplo3[0]);
+
+
+    //!Para compilar hay que usar g++ -I ../eigen-3.4.0/ lloyd.cpp -o lloyd.exe
+    // Documentación: https://eigen.tuxfamily.org/dox/group__TutorialMatrixClass.html
+
+    return 0;
+}
