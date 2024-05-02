5741212d0eaa76b13f9ca7c1de98313032a1d361
FIRST_NAME LAST_NAME <juamon05@ucm.es>
2024-05-02 12:51:48 +0200

    entrenamiento k-medias acabado creo

--- P3/K-medias/k-medias.cpp
+++ P3/K-medias/k-medias.cpp
@@ -16,8 +16,8 @@ using namespace std;
 double tolerancia = 0.01;
 double pesoExponencial = 2;
 
-Vector4d v1 (4.6, 3.0, 4.0, 0.0);
-Vector4d v2 (6.8, 3.4, 4.6, 0.7);
+const Vector4d V1 (4.6, 3.0, 4.0, 0.0);
+const Vector4d V2 (6.8, 3.4, 4.6, 0.7);
 
 
 vector<Vector4d> readIrisData(const string& filename) {
@@ -52,7 +52,7 @@ vector<Vector4d> readIrisData(const string& filename) {
     double prob = 0.5;
     for (int i = 0; i < irisDataSize; i++)
     {
-        probs[i] = 0.5;
+        probs.push_back(prob);
     }
     return probs;
  }
@@ -67,9 +67,9 @@ double calcularD(Vector4d x, Vector4d c) {
 }
 
 //Calcula la probabilidad de que x pertenezca al grupo del centro v.
-double calcularP(Vector4d x, Vector4d v) {
+double calcularP(Vector4d x, Vector4d v1, Vector4d v2) {
     double exponente = 1/(pesoExponencial-1);
-    double numerador = pow(1/(calcularD(x, v)), exponente);
+    double numerador = pow(1/(calcularD(x, v1)), exponente);
 
     double denominador = 0;
     denominador += pow(1/(calcularD(x, v1)), exponente);
@@ -92,22 +92,37 @@ Vector4d calcularV(vector<Vector4d> irisData, vector<double> probs) {
 
 }
 
+
 void entrenar_kMedias(vector<Vector4d> irisData){
     vector<double> probsv1 = ini_probs(irisData.size());
     vector<double> probsv2 = ini_probs(irisData.size());
     
-    Vector4d v1 = calcularV(irisData, probsv1);
-    
-    for (int i = 0; i < irisData.size(); i++)
-    {
-        double p = calcularP(irisData.at(i), v1);
-        probsv1.push_back(p);
+    Vector4d v1_new = V1;
+    Vector4d v2_new = V2;
+    Vector4d v1;
+    Vector4d v2;
 
-        p = calcularP(irisData.at(i), v2);
-        probsv2.push_back(p);   
+    while (calcularD(v1_new, v1) > tolerancia 
+            && calcularD(v2_new, v2) > tolerancia) {
+   // for(int i = 0; i < 10; i++){
+        
+        v1 = v1_new;
+        v2 = v2_new;
+        for (int j = 0; j < irisData.size(); j++) {
+            double p = calcularP(irisData.at(j), v1, v2);
+            probsv1[j]= p;
+
+            p = calcularP(irisData.at(j), v2, v1);
+            probsv2[j]= p;   
+        }
+
+        v1_new = calcularV(irisData, probsv1);
+        cout << "|v1= " << v1.transpose() << endl;
+        v2_new = calcularV(irisData, probsv2);
+        cout << "|v2= " << v2.transpose() << endl << endl;
+
     }
-    cout << "|v1= " << v1.transpose() << endl;
-    
+        
 
 
 }
